16c807edfba2fe5389846202a21742279108fe6b
Emma <emmadomeisen@gmail.com>
2025-06-27 09:24:19 +0200

    Implemented getting video path

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,8 @@
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer,Responder,get};
 use serde_derive::Deserialize;
+use actix_cors::Cors;
+use reqwest::header::USER_AGENT;
+use reqwest::Client;
 
 #[derive(Debug, Deserialize)]
 pub struct Params {
@@ -8,15 +11,27 @@ pub struct Params {
 
 #[get("/sharetik")]
 async fn ttconv(req: HttpRequest) -> HttpResponse {
+    let client: Client = reqwest::Client::new();
     let params = web::Query::<Params>::from_query(req.query_string()).unwrap();
-    HttpResponse::Ok().body(format!("{:?}", params.url))
+    println!("{}",params.url);
+    let res = match client
+        .get(params.url.clone())
+        .header(USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.7103.48 Safari/537.36")
+        .send()
+        .await {
+            Ok(res) => res,
+            Err(err) => panic!("Error: {}", err)
+        };
+    let respath = res.url().path();
+    HttpResponse::Ok().body(format!("{:?}", respath))
 }
 
-
-#[actix_web::main] // or #[tokio::main]
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(ttconv)
+        let cors = Cors::default()
+            .allow_any_origin();
+        App::new().wrap(cors).service(ttconv)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
